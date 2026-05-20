51fa5b5f36b4a5f5762d488ee502cb295492396a
t1m0t <timothee.baron@pm.me>
2026-05-20 22:03:49 +0200

    update

--- serverutil/middlewares.go
+++ serverutil/middlewares.go
@@ -4,55 +4,8 @@ import (
 	"context"
 	"crypto/rand"
 	"encoding/base64"
-	"errors"
-	"fmt"
 	"net/http"
-
-	"suppa-ahg-stack/common-golang/logger"
 )
-
-func EnsureSessionMiddleWare(next http.Handler, sessionName string, secure bool, logger *logger.FileLogger) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		_, err := r.Cookie(sessionName)
-
-		if errors.Is(err, http.ErrNoCookie) {
-			sessionID, err := GenerateSessionID()
-			if err != nil {
-				logger.Error(fmt.Sprintf("EnsureSession: couldn't generate session id, %v", err))
-				http.Error(w, "internal error", http.StatusInternalServerError)
-				return
-			}
-
-			http.SetCookie(w, &http.Cookie{
-				Name:     sessionName,
-				Value:    sessionID,
-				Path:     "/",
-				HttpOnly: true,
-				Secure:   secure,
-				SameSite: http.SameSiteLaxMode,
-			})
-		} else if err != nil {
-			// malformed cookie or parsing issue
-			logger.Error(fmt.Sprintf("EnsureSession: bad cookie, %v", err))
-			http.Error(w, "bad cookie", http.StatusBadRequest)
-			return
-		}
-
-		next.ServeHTTP(w, r)
-	})
-}
-
-func GenerateSessionID() (string, error) {
-	// 32 bytes = 256 bits entropy
-	b := make([]byte, 32)
-
-	if _, err := rand.Read(b); err != nil {
-		return "", err
-	}
-
-	// URL-safe, no padding
-	return base64.RawURLEncoding.EncodeToString(b), nil
-}
 
 func ClearSessionCookie(w http.ResponseWriter, sessionName string, secure bool) {
 	http.SetCookie(w, &http.Cookie{
